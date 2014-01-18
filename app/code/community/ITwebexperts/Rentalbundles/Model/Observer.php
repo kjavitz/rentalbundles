dbf9ea51a3be81aba3e9621be7be737ace0d9430
Anton Vlasenko <vlasenko.anton@gmail.com>
2014-01-18 12:27:06 +0300

    Added method ITwebexperts_Rentalbundles_Model_Observer::onProductCardAddAction We are going to use it to set the neccessary data (arrival date?) for reservation products like country when ordering a bundle kit.

---
+++ app/code/community/ITwebexperts/Rentalbundles/Model/Observer.php
@@ -0,0 +1,12 @@
+<?php
+class ITwebexperts_Rentalbundles_Model_Observer
+{
+    /**
+     * Handles reservation of bundle product.
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function onProductCardAddAction(Varien_Event_Observer $observer)
+    {
+    }
+}
