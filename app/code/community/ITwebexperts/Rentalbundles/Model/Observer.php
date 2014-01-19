2dd7b626ec3d9243ca9927d650384bbe3ec6910d
Anton Vlasenko <vlasenko.anton@gmail.com>
2014-01-19 11:11:33 +0300

    Added method ITwebexperts_Rentalbundles_Model_Observer::onProductCardAddAction It is intended to process the start and the end dates of reservation product(s).

--- app/code/community/ITwebexperts/Rentalbundles/Model/Observer.php
+++ app/code/community/ITwebexperts/Rentalbundles/Model/Observer.php
@@ -8,5 +8,13 @@ class ITwebexperts_Rentalbundles_Model_Observer
      */
     public function onProductCardAddAction(Varien_Event_Observer $observer)
     {
+        $request = Mage::app()->getRequest();
+        $params = $request->getParams();
+        if (isset($params['start_date']) && is_array($params) && isset($params['start_date'][0])) {
+            // hardcoded for now. Will add better processing later
+            $request
+                ->setParam('start_date', '01/06/2014')
+                ->setParam('end_date', '01/07/2014');
+        }
     }
 }
