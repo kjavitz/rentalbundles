431f9f3a49d44fcfa801bc6e190e93a3a70f7974
Anton Vlasenko <vlasenko.anton@gmail.com>
2014-01-24 20:06:42 +0300

    Added new product attribute "Consider as Country". We are going to use this attribute to detect countries among reservation products.

--- app/code/community/ITwebexperts/Rentalbundles/sql/rentalbundles_setup/mysql4-install-1.0.0.php
+++ app/code/community/ITwebexperts/Rentalbundles/sql/rentalbundles_setup/mysql4-install-1.0.0.php
@@ -2,8 +2,38 @@
 $installer = $this;
 $installer->startSetup();
 
-//demo 
-//Mage::getModel('core/url_rewrite')->setId(null);
-//demo 
+$groupLabel = 'Rental Bundles';
+
+Mage::getResourceModel('catalog/setup', 'catalog_setup')->addAttribute(
+    Mage_Catalog_Model_Product::ENTITY, 'rentalbundles_is_country',
+    array(
+        'group' => $groupLabel,
+        'label' => 'Consider as Country',
+        'type' => 'int',
+        'input' => 'select',
+        'source' => 'eav/entity_attribute_source_boolean',
+        'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
+        'visible' => true,
+        'required' => false,
+        'user_defined' => false,
+        'default' => 0,
+        'apply_to' => 'reservation',
+        'visible_on_front' => false,
+        'position' => 1,
+    )
+);
+
+$entityTypeId = $installer->getEntityTypeId('catalog_product');
+$attributeId  = $installer->getAttributeId('catalog_product', 'rentalbundles_is_country');
+
+
+$attributeSets = $installer->_conn->fetchAll('select * from '.$this->getTable('eav/attribute_set').' where entity_type_id=?', $entityTypeId);
+
+foreach ($attributeSets as $attributeSet) {
+    $setId = $attributeSet['attribute_set_id'];
+    $installer->addAttributeGroup($entityTypeId, $setId, $groupLabel);
+    $groupId = $installer->getAttributeGroupId($entityTypeId, $setId, $groupLabel);
+    $installer->addAttributeToGroup($entityTypeId, $setId, $groupId, $attributeId);
+}
+
 $installer->endSetup();
-	 
