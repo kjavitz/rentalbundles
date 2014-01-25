2697fdddc1b74fd7b34d251969927089d4a9b693
Anton Vlasenko <vlasenko.anton@gmail.com>
2014-01-25 16:21:58 +0300

    Removed observer. Because we don't use it.

--- app/code/community/ITwebexperts/Rentalbundles/Model/Observer.php
+++ app/code/community/ITwebexperts/Rentalbundles/Model/Observer.php
@@ -8,13 +8,23 @@ class ITwebexperts_Rentalbundles_Model_Observer
      */
     public function onProductCardAddAction(Varien_Event_Observer $observer)
     {
+        return;
         $request = Mage::app()->getRequest();
         $params = $request->getParams();
-        if (isset($params['start_date']) && is_array($params) && isset($params['start_date'][0])) {
-            // hardcoded for now. Will add better processing later
-            $request
-                ->setParam('start_date', '01/06/2014')
-                ->setParam('end_date', '01/07/2014');
+        if (isset($params['start_date']) && is_array($params['start_date'])) {
+            foreach ($params['start_date'] as $product => $date) {
+                $this->getHelper()->getSession()->addStartDate($product, $date);
+            }
         }
+    }
+
+    /**
+     * Returns module's default helper.
+     *
+     * @return ITwebexperts_Rentalbundles_Helper_Data
+     */
+    public function getHelper()
+    {
+        return Mage::helper('rentalbundles');
     }
 }
