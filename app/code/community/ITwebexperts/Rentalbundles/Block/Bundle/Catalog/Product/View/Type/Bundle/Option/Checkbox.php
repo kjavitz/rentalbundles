80193feb2c9d483fc8584f134a6c70c32954caa3
Anton Vlasenko <vlasenko.anton@gmail.com>
2014-02-02 18:00:29 +0300

    Improved detection of countires options in ITwebexperts_Rentalbundles_Block_Bundle_Catalog_Product_View_Type_Bundle_Option_Checkbox::_construct Now we check if there are any countries in current bundle, only then we change template to the custom one.

--- app/code/community/ITwebexperts/Rentalbundles/Block/Bundle/Catalog/Product/View/Type/Bundle/Option/Checkbox.php
+++ app/code/community/ITwebexperts/Rentalbundles/Block/Bundle/Catalog/Product/View/Type/Bundle/Option/Checkbox.php
@@ -3,17 +3,21 @@ class ITwebexperts_Rentalbundles_Block_Bundle_Catalog_Product_View_Type_Bundle_O
 {
 
     /**
-     * Set template
+     * Sets custom template for countries.
      *
      * @return void
      */
     protected function _construct()
     {
-        if (!$this->_getModuleHelper()->isKitProduct($this->getProduct())) {
-            return parent::_construct();
+        $product = parent::getProduct();
+        if ($product instanceof Mage_Catalog_Model_Product) {
+            if ($this->_getModuleHelper()->getOptionBySelectionType($product, ITwebexperts_Rentalbundles_Model_System_Config_Source_Type::TYPE_COUNTRY)) {
+                $this->setTemplate('rentalbundles/catalog/product/view/type/bundle/option/checkbox.phtml');
+                return;
+            }
         }
 
-        $this->setTemplate('rentalbundles/catalog/product/view/type/bundle/option/checkbox.phtml');
+        return parent::_construct();
     }
 
 
