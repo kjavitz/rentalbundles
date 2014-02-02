8e67fbedf54a18f402f5a4add827b553c8f02f2d
Anton Vlasenko <vlasenko.anton@gmail.com>
2014-02-02 17:56:38 +0300

    Now we hide SIM bundle options on FE. Obviously because we don't want to let customer choose them. This is done automatically by the observer method.

---
+++ app/code/community/ITwebexperts/Rentalbundles/Block/Bundle/Catalog/Product/View/Type/Bundle/Option/Multi.php
@@ -0,0 +1,31 @@
+<?php
+class ITwebexperts_Rentalbundles_Block_Bundle_Catalog_Product_View_Type_Bundle_Option_Multi extends ITwebexperts_Payperrentals_Block_Bundle_Catalog_Product_View_Type_Bundle_Option_Multi
+{
+    /**
+     * This method hides SIMs bundle options
+     * on frontend (product view page).
+     *
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        $product = parent::getProduct();
+        if ($product instanceof Mage_Catalog_Model_Product) {
+            if ($this->_getModuleHelper()->getOptionBySelectionType($product, ITwebexperts_Rentalbundles_Model_System_Config_Source_Type::TYPE_SIM)) {
+                return '';
+            }
+        }
+        return parent::_toHtml();
+    }
+
+
+    /**
+     * Returns default module helper.
+     *
+     * @return ITwebexperts_Rentalbundles_Helper_Data
+     */
+    public function _getModuleHelper()
+    {
+        return $this->helper('rentalbundles');
+    }
+}
