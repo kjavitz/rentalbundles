411ac963928fdcfbebe5cd8c67e94e55984a9e8f
Anton Vlasenko <vlasenko.anton@gmail.com>
2014-02-10 01:51:06 +0300

    Added ability to display arrival dates for countries in Magento admin.  Just go to admin panel -> sales orders -> Order View page and you will see arrival dates below each country in the order items list.  Because we need to display this info to admin.

--- app/code/community/ITwebexperts/Rentalbundles/Block/Adminhtml/Sales/Order/View/Items/Renderer/Bundle.php
+++ app/code/community/ITwebexperts/Rentalbundles/Block/Adminhtml/Sales/Order/View/Items/Renderer/Bundle.php
@@ -1,5 +1,51 @@
 <?php
+
 class ITwebexperts_Rentalbundles_Block_Adminhtml_Sales_Order_View_Items_Renderer_Bundle extends ITwebexperts_Payperrentals_Block_Adminhtml_Sales_Order_View_Items_Renderer_Bundle
 {
+    public function getValueHtml($item)
+    {
+        $result = parent::getValueHtml($item);
 
+        // If product isn't a country we
+        // don't want to display
+        // start/end dates for it.
+        if (!$this->_checkValidProduct($item)) {
+            return $result;
+        }
+
+        $option = $item->getProductOptions();
+        $startDate = $endDate = null;
+
+        if (isset($option['info_buyRequest']['start_date'])) {
+            $startDate = $option['info_buyRequest']['start_date'];
+        }
+
+        if (isset($option['info_buyRequest']['end_date'])) {
+            $endDate = $option['info_buyRequest']['end_date'];
+        }
+
+        if ($endDate) {
+            $result .= '<br />' . $this->__('Start Date: ') . $startDate;
+        }
+
+        if ($endDate) {
+            $result .= '<br />' . $this->__('End Date: ') . $endDate;
+        }
+        return $result;
+    }
+
+    protected function _checkValidProduct(Varien_Object $item)
+    {
+        $productId = $item->getProductId();
+        if (!$productId) {
+            return;
+        }
+
+        $product = Mage::getModel('catalog/product')->load($productId);
+        if (!$product->getId()) {
+            return;
+        }
+
+        return ITwebexperts_Rentalbundles_Model_System_Config_Source_Type::TYPE_COUNTRY == $product->getRentalbundlesType();
+    }
 }
