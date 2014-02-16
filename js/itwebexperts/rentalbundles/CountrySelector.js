560debba1c6a77ff62eb6a802aa390192f0d168b
Anton Vlasenko <vlasenko.anton@gmail.com>
2014-02-16 22:02:58 +0300

    Now arrival dates datepicker correctly handles dates. We need these changes because now datepicker is inline (used to be pop-up).

--- js/itwebexperts/rentalbundles/CountrySelector.js
+++ js/itwebexperts/rentalbundles/CountrySelector.js
@@ -8,6 +8,8 @@ var ITwebexperts_Rentalbundles_Country_Selector = Class.create({
     datepicker: null,
     datepickerCntr: null,
 
+    currentInput: null,
+
     initialize: function (config) {
         Object.extend(this.config, config);
         Event.observe(document, 'dom:loaded', this.onDomLoaded.bind(this));
@@ -18,6 +20,7 @@ var ITwebexperts_Rentalbundles_Country_Selector = Class.create({
     },
 
     onCountryChange: function (select, evt) {
+        this.hidePicker(evt);
         this[select.value ? 'onCountrySelect' : 'onCountryReset'](select);
     },
 
@@ -95,9 +98,7 @@ var ITwebexperts_Rentalbundles_Country_Selector = Class.create({
         var dateInput = this.getArrivalDateBySelect(select);
         if (dateInput) {
             this._removeValidation(dateInput);
-            if (dateInput.id && $jppr && $jppr.datepick && $jppr.datepick._clearDate) {
-                $jppr.datepick._clearDate('#' + dateInput.id);
-            }
+            dateInput.value = '';
         }
     },
 
@@ -150,8 +151,44 @@ var ITwebexperts_Rentalbundles_Country_Selector = Class.create({
         }
     },
 
-    showPicker: function(el, evt) {
+    setDate: function (date) {
+        if (!date) {
+            return;
+        }
+
+        var date = new Date(date);
+        $jppr('#countryDatePicker').datepick('selectDateTd', date);
+    },
+
+    showPicker: function (el, evt) {
+        this.currentInput = el;
+        this.setDate(el.value);
+        var cntr = '#' + this.datepickerCntr.id,
+            pos = $jppr('#' + el.id).position();
+        $jppr(cntr).css('top', (pos.top + 30) + 'px');
+        $jppr(cntr).css('left', (pos.left + 0) + 'px');
+        $jppr(cntr).fadeIn("fast");
         Element.show(this.datepickerCntr);
+    },
+
+    hidePicker: function (evt) {
+        Element.hide(this.datepickerCntr);
+    },
+
+    _hidePicker: function (evt) {
+        var el = Event.element(evt),
+            descendant = !!$(el).up('.datepick-inline');
+
+        if (descendant || /.+StartDate\d{1,}/.match(el.id)) {
+            return;
+        }
+
+        this.hidePicker(evt);
+    },
+
+    onDateSelect: function (value, date, inst) {
+        this.currentInput.value = value;
+        this.hidePicker();
     },
 
     init: function () {
@@ -160,14 +197,18 @@ var ITwebexperts_Rentalbundles_Country_Selector = Class.create({
         this.datepicker = $('countryDatePicker');
         this.datepickerCntr = $('countryDatePickerDiv');
 
-        jQuery('#countryDatePicker').datepick({});
+        $jppr('#countryDatePicker').datepick({
+            onSelect: this.onDateSelect.bind(this)
+        });
 
         this.arrivalDates.each(function(el) {
-            Event.observe(el, 'click', this.showPicker.bind(this, el));
-        }.bind(this))
+            Event.observe(el, 'focus', this.showPicker.bind(this, el));
+        }.bind(this));
 
         this.countrySelectors.each(function (el) {
             Event.observe(el, 'change', this.onCountryChange.bind(this, el));
         }.bind(this))
+
+        Event.observe(document, 'click', this._hidePicker.bind(this));
     }
 });
