84859a9c41f6a63b135fbfc0a9929b8ec1ec9fc6
Anton Vlasenko <vlasenko.anton@gmail.com>
2014-02-17 14:59:04 +0300

    Added rewrite of ITwebexperts_Payperrentals_AjaxController controller. We need this to modify the price amount on product view page. We have to take into account arrival dates for countries.

---
+++ app/code/community/ITwebexperts/Rentalbundles/controllers/AjaxController.php
@@ -0,0 +1,6 @@
+<?php
+require_once(Mage::getModuleDir('controllers', 'ITwebexperts_Payperrentals') . DS . 'AjaxController.php');
+
+class ITwebexperts_Rentalbundles_AjaxController extends ITwebexperts_Payperrentals_AjaxController
+{
+}
