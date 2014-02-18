0fb9875e958bd3204ef58630a6fa10e7b76db2c2
Anton Vlasenko <vlasenko.anton@gmail.com>
2014-02-19 00:22:44 +0300

    Cleaned up code in ITwebexperts_Rentalbundles_Block_Checkout_Cart_Item_Renderer_Bundle class. There is no need to keep not used code.

--- app/code/community/ITwebexperts/Rentalbundles/Block/Checkout/Cart/Item/Renderer/Bundle.php
+++ app/code/community/ITwebexperts/Rentalbundles/Block/Checkout/Cart/Item/Renderer/Bundle.php
@@ -12,19 +12,12 @@ class ITwebexperts_Rentalbundles_Block_Checkout_Cart_Item_Renderer_Bundle extend
     {
         $options = parent::getOptionList();
         $simCardsTitle = $this->_getSimCardsTitle();
-        $countriesTitle = $this->_getCountriesTitle();
         if (is_array($options) && ($optionsCount = count($options))) {
             for ($i = 0; $i < $optionsCount; $i++) {
                 if ($simCardsTitle && isset($options[$i]['label']) && (false !== stripos($options[$i]['label'], $simCardsTitle))) {
                     // We don't want to display SIM cards
                     // on FE so we should remove them
                     // from array
-                    unset($options[$i]);
-                }
-
-                if ($countriesTitle && isset($options[$i]['label']) && (false !== stripos($options[$i]['label'], $countriesTitle))) {
-                    // Unsetting countries
-                    // Because we will add custom renderer for them.
                     unset($options[$i]);
                 }
             }
@@ -41,26 +34,6 @@ class ITwebexperts_Rentalbundles_Block_Checkout_Cart_Item_Renderer_Bundle extend
     public function getModuleHelper()
     {
         return Mage::helper('rentalbundles');
-    }
-
-    /**
-     * Returns title for SIM card bundle option.
-     *
-     * @return string|null
-     */
-    protected function _getCountriesTitle()
-    {
-        $product = $this->getProduct();
-        if (!$product) {
-            return;
-        }
-
-        $option = $this->getModuleHelper()->getOptionBySelectionType($product, ITwebexperts_Rentalbundles_Model_System_Config_Source_Type::TYPE_COUNTRY);
-        if (!$option) {
-            return;
-        }
-
-        return $option->getDefaultTitle();
     }
 
     /**
